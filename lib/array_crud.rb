66a8ed7f7ddac3fac0cdb2efe37ab2b57a0dfdae
Marnie Robinson <marnielane@gmail.com>
2021-01-11 19:45:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ writing << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  writing = ["book", "page", "letter", "note"]
+  writing.unshift ""
 end
 
 def remove_element_from_end_of_array(array)
