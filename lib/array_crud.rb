1cb4e4dbb31eabdcf93ebf95655d05489dc60305
Marnie Robinson <marnielane@gmail.com>
2021-01-11 19:42:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   writing = ["book", "page", "letter", "note"]
-writing << "pen"
+writing << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
