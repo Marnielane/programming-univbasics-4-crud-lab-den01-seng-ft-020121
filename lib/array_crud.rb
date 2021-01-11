84d2db2766008669310f62a717b899ecd3e7cd91
Marnie Robinson <marnielane@gmail.com>
2021-01-11 19:50:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  writing = ["book", "page", "letter", "note"]
+  writing = ["wow", "page", "letter", "note"]
   writing.shift
 end
 
